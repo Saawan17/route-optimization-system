c55a695114aea6289a095d84acdc255f6e4f7a8e
Saawan k <saawank17@gmail.com>
2026-02-09 12:18:22 +0530

    Update QwqerDemoBackendApplication.java

--- src/main/java/com/qwqer/demo/QwqerDemoBackendApplication.java
+++ src/main/java/com/qwqer/demo/QwqerDemoBackendApplication.java
@@ -1,5 +1,6 @@
 package com.qwqer.demo;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -11,5 +12,11 @@ public class QwqerDemoBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(QwqerDemoBackendApplication.class, args);
     }
+
+    @PostConstruct
+    public void logPort() {
+        System.out.println("PORT ENV = " + System.getenv("PORT"));
+    }
+
 
 }
